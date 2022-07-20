b337e26b58f9f94623fe8dd54649e9be064cd688
feridnand bracho <ferdinand.bracho@gmail.com>
2022-07-20 08:33:01 -0500

    clase 12

---
+++ js/events.js
@@ -0,0 +1,43 @@
+// ! Demo change background
+// const clickButton = () =>{
+//     document.body.style.backgroundColor = `
+//     rgb(
+//         ${Math.floor(Math.random() * (255+1))},
+//         ${Math.floor(Math.random() * (255+1))},
+//         ${Math.floor(Math.random() * (255+1))})
+//     `
+// }
+
+// Eventos mas ussados
+// click
+// change
+// input
+// focus
+// blur
+
+// Eventos de mouse
+// click
+// contectmenu
+// dbclick
+// mousedown
+// mouseenter
+// mouseleave
+// mousemove
+// mouseover
+// mouseup
+
+// Eventos de teclado
+// keydown
+// keypress
+// keyup
+
+// Eventos de navegador
+// DOMContentLoaded
+// load
+// loadstart
+// offline
+// online
+// scroll
+
+// Otros
+// toggle
