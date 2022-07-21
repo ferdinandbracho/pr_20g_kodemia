d821e161813137ae1bf4744838014f43e49cc08b
feridnand bracho <ferdinand.bracho@gmail.com>
2022-07-21 12:27:16 -0500

    clase 12

--- js/events.js
+++ js/events.js
@@ -1,43 +1,70 @@
-// ! Demo change background
-// const clickButton = () =>{
-//     document.body.style.backgroundColor = `
-//     rgb(
-//         ${Math.floor(Math.random() * (255+1))},
-//         ${Math.floor(Math.random() * (255+1))},
-//         ${Math.floor(Math.random() * (255+1))})
-//     `
-// }
 
-// Eventos mas ussados
-// click
-// change
-// input
-// focus
-// blur
+// ! Eventos 
+    // ! Estos suceden sobre los elementos de nuestro dom y podemos capturar y reaccionar a ellos, manipularlos a nustros favor 
 
-// Eventos de mouse
-// click
-// contectmenu
-// dbclick
-// mousedown
-// mouseenter
-// mouseleave
-// mousemove
-// mouseover
-// mouseup
+    //! tipos de enventos 
+        // ! Eventos Automaticos 
+        // ! Eventos generados por usaurios 
 
-// Eventos de teclado
-// keydown
-// keypress
-// keyup
 
-// Eventos de navegador
-// DOMContentLoaded
-// load
-// loadstart
-// offline
-// online
-// scroll
+// ! Eventos en linea -> Online events 
 
-// Otros
-// toggle
+
+const funEvent = () => {
+    // ! Demo change background
+    // console.log('Estoy cambiando')
+    document.body.style.backgroundColor = `
+    rgb(
+        ${Math.floor(Math.random() * (255+1))},
+        ${Math.floor(Math.random() * (255+1))},
+        ${Math.floor(Math.random() * (255+1))})
+ `
+}
+
+
+//! Eventos mas ussados
+//? click
+//? change
+//? input
+//? focus
+//? blur
+
+//! Eventos de mouse
+//? click
+//? contextmenu
+//? dbclick
+//? mousedown
+//? mouseup
+//? mouseenter
+//? mouseleave
+//? mousemove
+//? mouseover
+
+//! Eventos de teclado
+//? keydown
+//? keypress
+//? keyup
+
+//! Eventos de navegador
+//? load
+//? scroll
+
+// ! EventListener 
+    // ! Es un metodo de los elementos del dom
+
+// let btn1 = document.querySelector('.btn1')
+
+// btn1.addEventListener('submit', funEvent)
+
+// ! event handlers 
+
+// ! Event object 
+
+let btn1 = document.querySelector('.btn1')
+console.log(btn1.value)
+
+btn1.addEventListener('click',  (e) => {
+    console.log(e)
+})
+
+// addEventListener(<nombre del evento como string>,<callback -> nos da acceso object event</callback>>)
